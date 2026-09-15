19c43368b444d002a0bbb6a6da4b7073b06e7e1a
thienng-it <thienng.it@gmail.com>
2026-09-15 23:22:18 +0700

    fix(brand): refresh browser tab icon

--- tests/test_branding.py
+++ tests/test_branding.py
@@ -9,5 +9,8 @@ def test_app_shell_uses_the_kobean_brand_mark_and_favicon():
 
     assert page.title.string == "Kobean Manga Colorizer"
     assert page.select_one('.logo-icon img[src="brand-icon.png"]')
-    assert page.select_one('link[rel="icon"][href="favicon.png"]')
+    assert page.select_one('link[rel="icon"][href="kobean-favicon.png"]')
+    assert page.select_one('link[rel="shortcut icon"][href="kobean-favicon.ico"]')
     assert Path("static/brand-icon.png").is_file()
+    assert Path("static/kobean-favicon.png").is_file()
+    assert Path("static/kobean-favicon.ico").is_file()
